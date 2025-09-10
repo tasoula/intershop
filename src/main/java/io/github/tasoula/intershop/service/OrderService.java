4a2253caf2ded20f61d41b4835b57aa0635f8308
tasoula <nastiakorotkikh@yandex.ru>
2025-09-11 00:12:46 +0300

    рефакторинг

--- src/main/java/io/github/tasoula/intershop/service/OrderService.java
+++ src/main/java/io/github/tasoula/intershop/service/OrderService.java
@@ -1,102 +1,38 @@
 package io.github.tasoula.intershop.service;
 
-import io.github.tasoula.intershop.dao.*;
+import io.github.tasoula.intershop.dao.CartItemRepository;
+import io.github.tasoula.intershop.dao.OrderItemRepository;
+import io.github.tasoula.intershop.dao.OrderRepository;
+import io.github.tasoula.intershop.dao.ProductRepository;
 import io.github.tasoula.intershop.dto.OrderDto;
 import io.github.tasoula.intershop.dto.ProductDto;
-import io.github.tasoula.intershop.model.*;
+import io.github.tasoula.intershop.model.CartItem;
+import io.github.tasoula.intershop.model.Order;
+import io.github.tasoula.intershop.model.OrderItem;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class OrderService {
-
     private final OrderRepository orderRepository;
-
     private final OrderItemRepository orderItemRepository;
     private final CartItemRepository cartItemRepository;
     private final ProductRepository productRepository;
 
-    private final TransactionalOperator transactionalOperator;
-
-    public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository, CartItemRepository cartItemRepository, ProductRepository productRepository, TransactionalOperator transactionalOperator) {
+    public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository, CartItemRepository cartItemRepository, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
         this.orderItemRepository = orderItemRepository;
         this.cartItemRepository = cartItemRepository;
         this.productRepository = productRepository;
-        this.transactionalOperator = transactionalOperator;
     }
-
-    @Transactional
-    public Mono<UUID> createOrder(UUID userId) {
-        Flux<CartItem> cartItems = cartItemRepository.findByUserId(userId);
-
-        return cartItems.collectList()
-                .flatMap(items -> {
-                    if (items.isEmpty()) {
-                        return Mono.empty(); // Возвращаем Mono.empty() если корзина пуста
-                    }
-
-                    Flux<Boolean> stockCheckResults = Flux.fromIterable(items)
-                            .flatMap(cartItem -> productRepository.findById(cartItem.getProductId())
-                                    .map(product -> product.getStockQuantity() >= cartItem.getQuantity()));
-
-                    return stockCheckResults.all(result -> result)
-                            .flatMap(allInStock -> {
-                                if (!allInStock) {
-                                    return Mono.empty();
-                                }
-
-                                Order order = new Order();
-                                order.setUserId(userId);
-                                order.setCreatedAt(Timestamp.from(Instant.now()));
-                                return orderRepository.save(order)
-                                        .flatMap(savedOrder -> {
-                                            UUID orderId = savedOrder.getId();
-
-                                            Flux<OrderItem> orderItemsFlux = Flux.fromIterable(items)
-                                                    .flatMap(cartItem -> productRepository.findById(cartItem.getProductId())
-                                                            .flatMap(product -> {
-                                                                OrderItem orderItem = new OrderItem();
-                                                                orderItem.setOrderId(orderId);
-                                                                orderItem.setProductId(cartItem.getProductId());
-                                                                orderItem.setPriceAtTimeOfOrder(product.getPrice());
-                                                                orderItem.setQuantity(cartItem.getQuantity());
-                                                                product.setStockQuantity(product.getStockQuantity() - cartItem.getQuantity());
-                                                                return productRepository.save(product)
-                                                                        .then(orderItemRepository.save(orderItem))
-                                                                        .thenReturn(orderItem); // Сохраняем измененный продукт
-                                                            }));
-
-                                            return orderItemsFlux.collectList()
-                                                    .flatMap(orderItems -> {
-                                                        BigDecimal totalAmount = orderItems.stream()
-                                                                .map(item -> item.getPriceAtTimeOfOrder().multiply(BigDecimal.valueOf(item.getQuantity())))
-                                                                .reduce(BigDecimal::add)
-                                                                .orElse(BigDecimal.valueOf(0));
-
-                                                        savedOrder.setTotalAmount(totalAmount);
-
-                                                        return orderRepository.save(savedOrder)
-                                                                // 32. Удаляем товары из корзины пользователя.
-                                                                .then(cartItemRepository.deleteByUserId(userId))
-                                                                // 33. Возвращаем orderId.
-                                                                .thenReturn(orderId); // Возвращаем orderId
-                                                    });
-                                        });
-                            });
-                });
-    }
-
-
-
 
     public Mono<OrderDto> getById(UUID id) {
         return orderRepository.findById(id)
@@ -109,20 +45,100 @@ public class OrderService {
                 .flatMap(this::convertToDto);
     }
 
+    @Transactional
+    public Mono<UUID> createOrder(UUID userId) {
+        // Получаем все элементы корзины пользователя
+        return cartItemRepository.findByUserId(userId)
+                .collectList()
+                .flatMap(cartItems -> {
+                    if (cartItems.isEmpty()) {
+                        // Если корзина пуста, возвращаем пустой Mono
+                        return Mono.empty();
+                    }
+                    return checkStockAvailability(cartItems)
+                            .flatMap(allInStock -> {
+                                if (!allInStock) {
+                                    // Если какого-то товара нет в нужном количестве, возвращаем пустой Mono
+                                    return Mono.empty();
+                                }
+                                // Создаем заказ и сохраняем его
+                                return createAndSaveOrder(userId, cartItems);
+                            });
+                });
+    }
 
-    private Mono<OrderDto> convertToDto(Order order) {
-        return orderItemRepository.findByOrderId(order.getId())
-                .flatMap(orderItem ->
-                        productRepository.findById(orderItem.getProductId())
-                                .map(product -> { // Используем map для прямого преобразования Product -> ProductDto
-                                    ProductDto dto = new ProductDto();
-                                    dto.setId(product.getId());
-                                    dto.setTitle(product.getTitle());
-                                    dto.setQuantity(orderItem.getQuantity());
-                                    dto.setPrice(orderItem.getPriceAtTimeOfOrder());
-                                    return dto; // Возвращаем заполненный dto
-                                })
+    private Mono<Boolean> checkStockAvailability(List<CartItem> cartItems) {
+        return Flux.fromIterable(cartItems)
+                .flatMap(cartItem -> productRepository.findById(cartItem.getProductId())
+                        .map(product -> product.getStockQuantity() >= cartItem.getQuantity())
                 )
+                .all(Boolean::booleanValue); // Проверяем, что все продукты есть в нужном количестве
+    }
+
+    private Mono<UUID> createAndSaveOrder(UUID userId, List<CartItem> cartItems) {
+        Order order = new Order();
+        order.setUserId(userId);
+        order.setCreatedAt(Timestamp.from(Instant.now()));
+
+        return orderRepository.save(order)
+                .flatMap(savedOrder -> {
+                    UUID orderId = savedOrder.getId();
+                    // Создаем элементы заказа и обновляем количество товара на складе
+                    return saveOrderItemsAndUpdateStock(cartItems, orderId)
+                            .flatMap(orderItems -> {
+                                // Вычисляем общую сумму заказа
+                                BigDecimal totalAmount = orderItems.stream()
+                                        .map(item -> item.getPriceAtTimeOfOrder().multiply(BigDecimal.valueOf(item.getQuantity())))
+                                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+                                savedOrder.setTotalAmount(totalAmount);
+                                // Сохраняем обновленный заказ, очищаем корзину и возвращаем ID заказа
+                                return orderRepository.save(savedOrder)
+                                        .then(cartItemRepository.deleteByUserId(userId))
+                                        .thenReturn(orderId);
+                            });
+                });
+    }
+
+    private Mono<List<OrderItem>> saveOrderItemsAndUpdateStock(List<CartItem> cartItems, UUID orderId) {
+        return Flux.fromIterable(cartItems)
+                .flatMap(cartItem -> productRepository.findById(cartItem.getProductId())
+                        .flatMap(product -> {
+                            // Создаем элемент заказа
+                            OrderItem orderItem = new OrderItem();
+                            orderItem.setOrderId(orderId);
+                            orderItem.setProductId(cartItem.getProductId());
+                            orderItem.setPriceAtTimeOfOrder(product.getPrice());
+                            orderItem.setQuantity(cartItem.getQuantity());
+
+                            // Обновляем количество товара на складе
+                            product.setStockQuantity(product.getStockQuantity() - cartItem.getQuantity());
+
+                            // Сохраняем обновленный продукт и элемент заказа
+                            return productRepository.save(product)
+                                    .then(orderItemRepository.save(orderItem))
+                                    .thenReturn(orderItem);
+                        })
+                )
+                .collectList();
+    }
+
+
+    private Mono<ProductDto> convertToProductDto(OrderItem orderItem) {
+        return productRepository.findById(orderItem.getProductId())
+                .map(product -> {
+                    ProductDto dto = new ProductDto();
+                    dto.setId(product.getId());
+                    dto.setTitle(product.getTitle());
+                    dto.setQuantity(orderItem.getQuantity());
+                    dto.setPrice(orderItem.getPriceAtTimeOfOrder());
+                    return dto;
+                });
+    }
+
+    public Mono<OrderDto> convertToDto(Order order) {
+        return orderItemRepository.findByOrderId(order.getId())
+                .flatMap(this::convertToProductDto)  // Используем отдельный метод для преобразования
                 .collectList()
                 .map(productDtos -> {
                     BigDecimal totalAmount = productDtos.stream()
