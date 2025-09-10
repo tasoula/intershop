08126eff0b41ff0a8bd592bc116e9b2ff55bd346
tasoula <nastiakorotkikh@yandex.ru>
2025-09-10 23:48:03 +0300

    исправлены тесты OrderIntemRepository

--- src/test/java/io/github/tasoula/intershop/dao/OrderItemRepositoryTest.java
+++ src/test/java/io/github/tasoula/intershop/dao/OrderItemRepositoryTest.java
@@ -16,53 +16,82 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OrderItemRepositoryTest extends SpringBootPostgreSQLBase{
     @Autowired
-    private OrderRepository orderRepository;
-
-    @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired OrderItemRepository orderItemRepository;
 
-    private Order order1;
-    private Order order2;
-    private UUID userId;
+    private UUID orderId;
+    private OrderItem orderItem1;
+    private OrderItem orderItem2;
 
     @BeforeEach
     void setUp() {
+        orderItemRepository.deleteAll().block();
         orderRepository.deleteAll().block();
+        productRepository.deleteAll().block();
         userRepository.deleteAll().block();
 
         User user =  new User();
         user.setCreatedAt(Timestamp.from(Instant.now()));
-        userId = userRepository.save(user).block().getId();
+        UUID userId = userRepository.save(user).block().getId();
 
-        order1 = new Order();
-        order1.setUserId(userId);
-        order1.setCreatedAt(Timestamp.from(Instant.now()));
-        order1.setTotalAmount(BigDecimal.valueOf(100));
+        Product product1 = new Product();
+        product1.setTitle("Amazing Product");
+        product1.setDescription("This is an amazing product description.");
+        product1.setPrice(new BigDecimal("25.00"));
+        product1.setStockQuantity(10);
+        product1.setImgPath("/images/product1.jpg");
+        UUID productId1 = productRepository.save(product1).block().getId();
 
-        order2 = new Order();
-        order2.setUserId(userId);
-        order2.setCreatedAt(Timestamp.from(Instant.now()));
-        order2.setTotalAmount(BigDecimal.valueOf(200));
+        Product product2 = new Product();
+        product2.setTitle("Another Product");
+        product2.setDescription("Another great product.");
+        product2.setPrice(new BigDecimal("50.00"));
+        product2.setStockQuantity(5);
+        UUID productId2 = productRepository.save(product2).block().getId();
+
+        Order order = new Order();
+        order.setUserId(userId);
+        order.setCreatedAt(Timestamp.from(Instant.now()));
+        order.setTotalAmount(BigDecimal.valueOf(100));
+        orderId = orderRepository.save(order).block().getId();
+
+        orderItem1 = new OrderItem();
+        orderItem1.setOrderId(orderId);
+        orderItem1.setProductId(productId1);
+        orderItem1.setQuantity(5);
+        orderItem1.setPriceAtTimeOfOrder(BigDecimal.TEN);
+
+        orderItem2 = new OrderItem();
+        orderItem2.setOrderId(orderId);
+        orderItem2.setProductId(productId2);
+        orderItem2.setQuantity(5);
+        orderItem2.setPriceAtTimeOfOrder(BigDecimal.valueOf(101));
     }
 
     @Test
     void saveAndFindOrdersByUserId_WhenExists() {
-        var orders = orderRepository.saveAll(List.of(order1, order2))
-                .thenMany(orderRepository.findByUserId(userId))
+        var orderItems = orderItemRepository.saveAll(List.of(orderItem1, orderItem2))
+                .thenMany(orderItemRepository.findByOrderId(orderId))
                 .toIterable();
 
-        assertThat(orders)
-                .withFailMessage("Заказы не найдены")
+        assertThat(orderItems)
+                .withFailMessage("Позиции заказа не найдены")
                 .isNotEmpty()
-                .withFailMessage("неверное количество заказов")
-                .hasSize(2);
+                .withFailMessage("неверное количество позиций в заказе")
+                .hasSize(2)
+                 .first()
+                .withFailMessage("первая позиция должна быть: " + orderItem1.getId())
+                .extracting(OrderItem::getId)
+                .isEqualTo(orderItem1.getId());
     }
 
     @Test
     void saveAndFindOrdersByUserId_WhenNotExists() {
-        var orders = orderRepository.saveAll(List.of())
-                .thenMany(orderRepository.findByUserId(userId))
-                .toIterable();
+        var orders = orderItemRepository.findByOrderId(UUID.randomUUID()).toIterable();
 
         assertThat(orders)
                 .withFailMessage("Заказы не должны быть найдены")
