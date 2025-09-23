278d49ed7f3ce178d15312b4b82f84c1efe0fff9
tasoula <nastiakorotkikh@yandex.ru>
2025-09-23 13:59:25 +0300

    рефакторинг

--- store/src/main/java/io/github/tasoula/intershop/service/ProductDataService.java
+++ store/src/main/java/io/github/tasoula/intershop/service/ProductDataService.java
@@ -3,7 +3,6 @@ package io.github.tasoula.intershop.service;
 import io.github.tasoula.intershop.dao.ProductRepository;
 import io.github.tasoula.intershop.exceptions.ResourceNotFoundException;
 import io.github.tasoula.intershop.model.Product;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
@@ -15,7 +14,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,6 +23,8 @@ public class ProductDataService {
 
     private final ReactiveRedisTemplate<String, Product> reactiveRedisTemplate; // Inject ReactiveRedisTemplate
     private final String ALL_PRODUCTS_CACHE_KEY_PREFIX = "products_all";
+
+    private final String PRODUCTS_CACHE_KEY_PREFIX = "products";
 
     public ProductDataService(ProductRepository repository, ImageService imageService, ReactiveRedisTemplate<String, Product> reactiveRedisTemplate) {
         this.productRepository = repository;
@@ -39,10 +39,9 @@ public class ProductDataService {
                 .hasElements() // Check if the Redis list has any elements
                 .flatMapMany(hasElements -> {
                     if (hasElements) {
-                        System.out.println("----------------Getting data from cache " + cacheKey);
                         return reactiveRedisTemplate.opsForList().range(cacheKey, 0, -1); // Return cached data
                     } else {
-                        return find(search, pageable, cacheKey)
+                        return getFromRepository(search, pageable)
                                 .collectList()
                                 .flatMapMany(products -> {
                                     System.out.println("----------------Chaching data " + cacheKey);
@@ -54,19 +53,15 @@ public class ProductDataService {
                 });
     }
 
-    private Flux<Product> find(String search, Pageable pageable, String cacheKey){
-        System.out.println("-----------------No data in chace, getting from db "  + cacheKey);
-        return (search == null || search.isEmpty())
-                ? productRepository.findByStockQuantityGreaterThan(0, pageable)
-                : productRepository.findByTitleContainingOrDescriptionContainingIgnoreCaseAndStockQuantityGreaterThan(
-                search.toLowerCase(), search.toLowerCase(), 0, pageable);
-    }
-
-    @Cacheable(value = "products", key = "#productId")
+    @Cacheable(value = PRODUCTS_CACHE_KEY_PREFIX, key = "#productId")
     public Mono<Product> findById(UUID productId) {
         System.out.println("-------Getting product " + productId + " from db");
         return productRepository.findById(productId)
                 .switchIfEmpty(Mono.error(new ResourceNotFoundException("Product not found with id: " + productId)));
+    }
+
+    public Mono<Long> count(){
+        return productRepository.count();
     }
 
     @Transactional
@@ -77,20 +72,8 @@ public class ProductDataService {
                 .then(clearAllProductsCache()); // Очищаем кеш после создания продукта
     }
 
-    private Mono<Void> clearAllProductsCache() {
-        System.out.println("-------Clearing products_all ");
-        return reactiveRedisTemplate.keys(ALL_PRODUCTS_CACHE_KEY_PREFIX + "*")  // Получаем все ключи, начинающиеся с префикса
-                .flatMap(reactiveRedisTemplate::delete) // Удаляем каждый ключ
-                .then(); // Возвращаем Mono<Void>
-    }
-
-    public Mono<Long> count(){
-        return productRepository.count();
-    }
-
-    @CachePut(value = "products", key = "#product.id")
+    @CachePut(value = PRODUCTS_CACHE_KEY_PREFIX, key = "#product.id")
     public Mono<Product> update(Product product){
-        System.out.println("-------Updating product " + product.getId());
         return productRepository.existsById(product.getId())
                 .flatMap(exists -> {
                     if(exists){
@@ -101,4 +84,18 @@ public class ProductDataService {
                 .switchIfEmpty(Mono.error(() -> new ResourceNotFoundException("Product with id " + product.getId() + " not found.")));
 
     }
+
+    private Flux<Product> getFromRepository(String search, Pageable pageable){
+        return (search == null || search.isEmpty())
+                ? productRepository.findByStockQuantityGreaterThan(0, pageable)
+                : productRepository.findByTitleContainingOrDescriptionContainingIgnoreCaseAndStockQuantityGreaterThan(
+                search.toLowerCase(), search.toLowerCase(), 0, pageable);
+    }
+
+    private Mono<Void> clearAllProductsCache() {
+        return reactiveRedisTemplate.keys(ALL_PRODUCTS_CACHE_KEY_PREFIX + "*")  // Получаем все ключи, начинающиеся с префикса
+                .flatMap(reactiveRedisTemplate::delete) // Удаляем каждый ключ
+                .then(); // Возвращаем Mono<Void>
+    }
+
 }
