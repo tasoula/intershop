212dfde2fa67fa9d792615991b1ef7621cde91b4
tasoula <nastiakorotkikh@yandex.ru>
2025-09-24 23:30:52 +0300

    Добавлено развертывание приложения в Docker

--- store/src/main/java/io/github/tasoula/intershop/config/RedisConfig.java
+++ store/src/main/java/io/github/tasoula/intershop/config/RedisConfig.java
@@ -1,13 +1,11 @@
 package io.github.tasoula.intershop.config;
 
 import io.github.tasoula.intershop.model.Product;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -15,7 +13,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
 
 @Configuration
 public class RedisConfig {
