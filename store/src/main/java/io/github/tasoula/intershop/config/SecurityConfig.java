36dadf18571015d1cba07b4916813139c38f45cb
tasoula <nastiakorotkikh@yandex.ru>
2025-10-07 15:47:21 +0300

    Сокрытие кнопок корзины для неаутентифицированных пользователей. Ссылка на странице логина (пока почему-то не получается отображать ее только для неаутентифицированных пользователей)

--- store/src/main/java/io/github/tasoula/intershop/config/SecurityConfig.java
+++ store/src/main/java/io/github/tasoula/intershop/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
@@ -21,6 +22,7 @@ import java.net.URI;
 
 @Configuration
 @EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
