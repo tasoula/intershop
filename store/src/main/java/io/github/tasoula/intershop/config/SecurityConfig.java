4c3b1b9c13a24a27c38ebb04cb8ef0a89ac7a1bc
tasoula <nastiakorotkikh@yandex.ru>
2025-10-10 17:19:19 +0300

    Изменен алгоритм шифрования паролей

--- store/src/main/java/io/github/tasoula/intershop/config/SecurityConfig.java
+++ store/src/main/java/io/github/tasoula/intershop/config/SecurityConfig.java
@@ -7,10 +7,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
@@ -79,5 +76,10 @@ public class SecurityConfig {
             exchange.getResponse().getHeaders().setLocation(URI.create("/catalog/items"));
             return exchange.getResponse().setComplete();
         };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- store/src/main/java/io/github/tasoula/intershop/controller/LoginController.java
+++ store/src/main/java/io/github/tasoula/intershop/controller/LoginController.java
@@ -5,6 +5,7 @@ import io.github.tasoula.intershop.model.User;
 import io.github.tasoula.intershop.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -21,8 +22,11 @@ public class LoginController {
 
     private final UserService userDetailService;
 
-    public LoginController(UserService userDetailService) {
+    private final PasswordEncoder passwordEncoder;
+
+    public LoginController(UserService userDetailService, PasswordEncoder passwordEncoder) {
         this.userDetailService = userDetailService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/login")
@@ -49,10 +53,9 @@ public class LoginController {
 
     @PostMapping("/register")
     public Mono<String> create(@ModelAttribute UserRegistrationDto userRegistrationDto, Model model) {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         User user = new User();
         user.setUserName(userRegistrationDto.getUsername());
-        user.setPassword(encoder.encode(userRegistrationDto.getPassword()));
+        user.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
         List<String> authorities = userRegistrationDto.isAdmin() ? List.of("ROLE_USER")
                 : List.of("ROLE_ADMIN", "ROLE_USER");
 
