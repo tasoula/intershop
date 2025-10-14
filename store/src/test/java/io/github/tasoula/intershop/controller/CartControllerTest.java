149638f8769b609308d7700833a8fa313ebf494c
tasoula <nastiakorotkikh@yandex.ru>
2025-10-14 16:15:35 +0300

    CartControllerTest

--- store/src/test/java/io/github/tasoula/intershop/controller/CartControllerTest.java
+++ store/src/test/java/io/github/tasoula/intershop/controller/CartControllerTest.java
@@ -1,14 +1,19 @@
 package io.github.tasoula.intershop.controller;
 
 import io.github.tasoula.intershop.dto.ProductDto;
-import io.github.tasoula.intershop.interceptor.UserInterceptor;
+import io.github.tasoula.intershop.model.User;
 import io.github.tasoula.intershop.service.CartService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
@@ -18,13 +23,13 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-import static io.github.tasoula.intershop.interceptor.CookieConst.USER_ID;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf;
 
-@WebFluxTest(value = CartController.class,
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = UserInterceptor.class))
+@WebFluxTest(CartController.class)
+@AutoConfigureWebTestClient
 class CartControllerTest {
 
     @Autowired
@@ -33,10 +38,22 @@ class CartControllerTest {
     @MockitoBean
     private CartService cartService;
 
+    private static UUID userId = UUID.randomUUID();
+    private static Authentication userAuthentication;
+
+    @BeforeAll
+    static void beforeAll() {
+        userId = UUID.randomUUID();
+        User mockUser = new User();
+        mockUser.setId(userId);
+        mockUser.setUserName("testuser");
+        mockUser.setPassword("password");
+        mockUser.setAuthorities(List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        userAuthentication = new UsernamePasswordAuthenticationToken(mockUser, null, mockUser.getAuthorities());
+    }
 
     @Test
     void viewCart_shouldReturnCartViewWithItemsAndTotal() throws Exception {
-        UUID userId = UUID.randomUUID();
         List<ProductDto> items = List.of(new ProductDto(
                                                 UUID.randomUUID(),
                                                 "Product 1",
@@ -50,12 +67,9 @@ class CartControllerTest {
         when(cartService.calculateTotalPriceByUserId(userId)).thenReturn(Mono.just(total));
         when(cartService.isAvailable(userId)).thenReturn(Mono.just(true));
 
-        webTestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/cart/items")
-                        .queryParam("userId", userId.toString())
-                        .build())
-                .cookie(USER_ID, userId.toString())
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockAuthentication(userAuthentication))
+                .get()
+                .uri("/cart/items")
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentTypeCompatibleWith(MediaType.TEXT_HTML)
@@ -67,26 +81,20 @@ class CartControllerTest {
                     assertTrue(body.contains(items.getFirst().getTitle()), "product1 not found in template");
                     assertTrue(body.contains("<b>100.0 руб.</b>"), "product1 price not found in template");
                     assertTrue(body.contains("<b id=\"total-price\">Итого: 200.0 руб.</b>"), "total summ not found in template");
-
                 });
     }
 
-
     @Test
     void viewCart_shouldReturnCartViewWithEmptyCart() throws Exception {
-        UUID userId = UUID.randomUUID();
         List<ProductDto> items = List.of();
         BigDecimal total = BigDecimal.ZERO;
 
         when(cartService.findByUserId(userId)).thenReturn(Flux.fromIterable(items));
         when(cartService.calculateTotalPriceByUserId(userId)).thenReturn(Mono.just(total));
 
-        webTestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/cart/items")
-                        .queryParam("userId", userId.toString())
-                        .build())
-                .cookie(USER_ID, userId.toString())
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockAuthentication(userAuthentication))
+                .get()
+                .uri("/cart/items")
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentTypeCompatibleWith(MediaType.TEXT_HTML)
@@ -96,20 +104,18 @@ class CartControllerTest {
                     String body = new String(result.getResponseBodyContent());
                     assertTrue(body.contains("<title>Корзина товаров</title>"), "page title not found in template");
                     assertTrue(body.contains("<b id=\"total-price\">Итого: 0 руб.</b>"), "total summ not found in template");
-
                 });
     }
 
     @Test
     void getTotal_returnsTotalPrice() {
-        UUID userId = UUID.randomUUID();
         BigDecimal total = BigDecimal.valueOf(42.50);
 
         when(cartService.calculateTotalPriceByUserId(userId)).thenReturn(Mono.just(total));
 
-        webTestClient.get()
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockAuthentication(userAuthentication))
+                .get()
                 .uri("/cart/total")
-                .cookie(USER_ID, userId.toString())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(BigDecimal.class)
@@ -118,13 +124,11 @@ class CartControllerTest {
 
     @Test
     void isEmpty_returnsTrueWhenCartIsEmpty() {
-        UUID userId = UUID.randomUUID();
-
         when(cartService.isEmpty(userId)).thenReturn(Mono.just(true));
 
-        webTestClient.get()
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockAuthentication(userAuthentication))
+                .get()
                 .uri("/cart/is_empty")
-                .cookie(USER_ID, userId.toString())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Boolean.class)
@@ -133,13 +137,11 @@ class CartControllerTest {
 
     @Test
     void isEmpty_returnsFalseWhenCartIsNotEmpty() {
-        UUID userId = UUID.randomUUID();
-
         when(cartService.isEmpty(userId)).thenReturn(Mono.just(false));
 
-        webTestClient.get()
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockAuthentication(userAuthentication))
+                .get()
                 .uri("/cart/is_empty")
-                .cookie(USER_ID, userId.toString())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Boolean.class)
@@ -148,15 +150,15 @@ class CartControllerTest {
 
     @Test
     void changeProductQuantityInCart_plusAction_returnsUpdatedQuantity() {
-        UUID userId = UUID.randomUUID();
         UUID productId = UUID.randomUUID();
         int updatedQuantity = 5;
 
         when(cartService.changeProductQuantityInCart(userId, productId, 1)).thenReturn(Mono.just(updatedQuantity));
 
-        webTestClient.post()
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockAuthentication(userAuthentication))
+                .mutateWith(csrf())
+                .post()
                 .uri("/cart/items/{id}?action=PLUS", productId)
-                .cookie(USER_ID, userId.toString())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Integer.class)
@@ -165,15 +167,15 @@ class CartControllerTest {
 
     @Test
     void changeProductQuantityInCart_minusAction_returnsUpdatedQuantity() {
-        UUID userId = UUID.randomUUID();
         UUID productId = UUID.randomUUID();
         int updatedQuantity = 3;
 
         when(cartService.changeProductQuantityInCart(userId, productId, -1)).thenReturn(Mono.just(updatedQuantity));
 
-        webTestClient.post()
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockAuthentication(userAuthentication))
+                .mutateWith(csrf())
+                .post()
                 .uri("/cart/items/{id}?action=MINUS", productId)
-                .cookie(USER_ID, userId.toString())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Integer.class)
@@ -182,18 +184,72 @@ class CartControllerTest {
 
     @Test
     void changeProductQuantityInCart_deleteAction_returnsOkZero() {
-        UUID userId = UUID.randomUUID();
         UUID productId = UUID.randomUUID();
 
         when(cartService.deleteCartItem(userId, productId)).thenReturn(Mono.empty());
 
-        webTestClient.post()
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockAuthentication(userAuthentication))
+                .mutateWith(csrf())
+                .post()
                 .uri("/cart/items/{id}?action=DELETE", productId)
-                .cookie(USER_ID, userId.toString())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Integer.class)
                 .isEqualTo(0);
     }
 
+
+    @Test
+    void viewCart_accessDeniedForUnauthentificated() {
+        webTestClient.get()
+                .uri("/cart/items")
+                .exchange()
+                .expectStatus().isFound()
+                .expectHeader().location("/login"); // Expect redirect
+
+        Mockito.verifyNoInteractions(cartService); // Verify that the service method was never called
+    }
+
+    @Test
+    void getTotal_accessDeniedForUnauthentificated() {
+        webTestClient.get()
+                .uri("/cart/total")
+                .exchange()
+                .expectStatus().isFound()
+                .expectHeader().location("/login"); // Expect redirect
+
+        Mockito.verifyNoInteractions(cartService); // Verify that the service method was never called
+    }
+
+    @Test
+    void isEmpty_accessDeniedForUnauthentificated() {
+        webTestClient.get()
+                .uri("/cart/is_empty")
+                .exchange()
+                .expectStatus().isFound()
+                .expectHeader().location("/login"); // Expect redirect
+
+        Mockito.verifyNoInteractions(cartService); // Verify that the service method was never called
+    }
+
+    @Test
+    void isAvailable_accessDeniedForUnauthentificated() {
+        webTestClient.get()
+                .uri("/cart/is_available")
+                .exchange()
+                .expectStatus().isFound()
+                .expectHeader().location("/login"); // Expect redirect
+
+        Mockito.verifyNoInteractions(cartService); // Verify that the service method was never called
+    }
+
+    @Test
+    void changeProductQuantityInCart_accessDeniedForUnauthentificated() {
+        webTestClient.post()
+                .uri("/cart/items/{id}?action=PLUS", UUID.randomUUID())
+                .exchange()
+                .expectStatus().isForbidden();
+
+        Mockito.verifyNoInteractions(cartService); // Verify that the service method was never called
+    }
 }
